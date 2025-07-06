53fd122edd83dd59b48417765d794e102cd5b2c8
MENDU-SREEKANTH-REDDY <143696588+MENDU-SREEKANTH-REDDY@users.noreply.github.com>
2025-07-06 18:48:54 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import TopBar from "./components/TopBar";
+import ToolBar from "./components/ToolBar";
+import TabNav from "./components/TabNav";
+import TableSection from "./components/TableSection";
+
+export default function App() {
+  return (
+    <>
+      <TopBar />
+      <ToolBar /> 
+      <TableSection />
+      <TabNav />
+    </>
+  );
+}
